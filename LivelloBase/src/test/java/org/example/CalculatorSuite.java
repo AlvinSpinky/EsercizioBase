3df0c1e973881411dea3cdaebcd616e9884d9fd5
AlvinSpinky <ddrndr02d27d086h@studenti.unical.it>
2024-11-26 11:17:28 +0100

    Letsgoski

---
+++ LivelloBase/src/test/java/org/example/CalculatorSuite.java
@@ -0,0 +1,8 @@
+package org.example;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorTest.class})
+public class CalculatorSuite {
+}
